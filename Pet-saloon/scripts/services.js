85d961d468a68dc7936af59f12f43256fddf6ec2
Guillermo Escobar <155601587+Modelo1911@users.noreply.github.com>
2024-02-17 16:18:48 +0000

    last update

--- Pet-saloon/scripts/services.js
+++ Pet-saloon/scripts/services.js
@@ -1,3 +1,5 @@
+let petService=0;
+
 var services=[];//this is an array
 
 $(document).ready(function(){
@@ -32,5 +34,35 @@ function displayItems(items){//items is the object, inside the () is the array a
         li=`<li>${item.description} -$ ${item.price}</li>`;//description is the property
         htmlList.append(li);
     }
+}
+
+    function getE(id){
+        return document.getElementById(id);
+    }
+
+    function isValid(aService){
+        let validation=true;
+
+        getE("#txtService").classList.remove("alert-error");
+        getE("#txtPrice").classList.remove("alert-error");
+    
+    if(item.service==""){
+        validation=false;
+        getE("#txtService").classList.add("alert-error");
+    }
+    if(item.price==""){
+        validation=false;
+        getE("#txtPrice").classList.add("alert-error");
+    }
+    return validation;
+}
+
+function showNotifications(msg,type){
+    getE("notifications").classList.remove("hidden");
+    getE("notifications").innerHTML=`<p class="${type}">${msg}</p>`;
+
+    setTimeout(function(){
+        getE("notifications").classList.add("hidden");
+    },4000);
 }
 
